0d7fe675d36da20ddb159eadababd0b15b5bd31c
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-01-31 16:45:50 -0500

    Adding second logict part

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
  * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
+ * DONE: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses
-
+let guesses = [];
 // Variable for store the correct random number
 
 let correctNumber = getRandomNumber();
@@ -30,6 +30,8 @@ function playGame() {
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById("number-guess").value;
   displayResult(numberGuess);
+  saveGuessHistory(numberGuess);
+  displayHistory();
 }
 
 /**
@@ -80,6 +82,7 @@ function getRandomNumber() {
  */
 function saveGuessHistory(guess) {
   // *CODE GOES BELOW HERE *
+  guesses.push(guess);
 }
 
 /**
@@ -91,9 +94,17 @@ function saveGuessHistory(guess) {
  * HINT: use while loop and string concatentation to create a list of guesses
  */
 function displayHistory() {
-  let index; // TODO
+  let index = guesses.length - 1; // TODO
   let list = "<ul class='list-group'>";
   // *CODE GOES BELOW HERE *
+  while (index >= 0) {
+    list +=
+      "<li class='list-group-item'>" +
+      "You guessed " +
+      guesses[index] +
+      "</li>";
+      index--;
+  }
   list += "</ul>";
   document.getElementById("history").innerHTML = list;
 }
